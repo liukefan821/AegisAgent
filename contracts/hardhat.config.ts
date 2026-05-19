3e5cf50bbfeec7922368c457bc869d3bb79d1d65
zhu-ruiqi <206345965+zhu-ruiqi@users.noreply.github.com>
2026-05-19 14:04:13 +0800

    chore: add deploy script and config, successfully deployed and verified on sepolia

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,12 +1,25 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   gasReporter: {
-    enabled: true, // 必须设为 true 才会显示报告
-    currency: "USD", // 可选：将 Gas 换算成美元
-    gasPrice: 20, // 可选：设置平均 Gas 价格 (Gwei)
+    enabled: true,
+    currency: "USD",
+    gasPrice: 20,
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
